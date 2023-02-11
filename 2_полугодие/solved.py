c1fe009785a07f197b578c85dde4075f21504f99
Aakookie <114381882+Aakookie@users.noreply.github.com>
2023-02-11 08:54:00 +0700

    Update solved.py

--- 2_полугодие/solved.py
+++ 2_полугодие/solved.py
@@ -20,3 +20,11 @@ for N in range(516):
     if int(b, 2) > 516:
         print(N)
         break
+#23
+def f(x, y):
+    if x > y or x == 17:
+        return(0)
+    if x == y:
+        return(1)
+    return f(x+1, y) + f (x*2, y)
+print(f(1, 10)*f(10, 35))
