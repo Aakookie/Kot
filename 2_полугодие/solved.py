451df107ee01ba3e02db09a46c8ca0ab11f132e7
Aakookie <114381882+Aakookie@users.noreply.github.com>
2023-02-25 12:59:43 +0700

    Update solved.py

--- 2_полугодие/solved.py
+++ 2_полугодие/solved.py
@@ -53,13 +53,13 @@ for i in range(len(a)-1):
 print(count, maxsum)
 
 #23
-def f(x, y):
-    if x > y or x == 17:
-        return(0)
-    if x == y:
-        return(1)
-    return f(x+1, y) + f (x*2, y)
-print(f(1, 10)*f(10, 35))
+def f(x,y):
+    if x>y or x==17:
+        return 0
+    elif x==y:
+        return 1
+    return f(x+1,y) +f(x*2,y)
+print(f(1,10)*f(10,35))
 
 #24
 with open('24.txt') as f:
